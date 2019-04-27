52596a750ca8af8f290effaeae6068f14c917661
Momin Khan <foolmoron@gmail.com>
2019-04-27 13:35:57 -0400

    rain

--- Assets/Scripts/SpawnEntities.cs
+++ Assets/Scripts/SpawnEntities.cs
@@ -15,11 +15,15 @@ public class SpawnEntities : MonoBehaviour {
   public Vector3 RotationMin;
   public Vector3 RotationMax;
 
-  void Start() {
-    var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
-    var em = World.Active.EntityManager;
+  Entity prefab;
 
-    for (int i = 0; i < Count; i++) {
+  void Start() {
+    prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
+  }
+
+  void FixedUpdate() {
+    var em = World.Active.EntityManager;
+    for (int i = 0; i < 2; i++) {
       var e = em.Instantiate(prefab);
       em.SetComponentData(e, new Translation { Value = new Vector3(Mathf.Lerp(PositionMin.x, PositionMax.x, Random.value), Mathf.Lerp(PositionMin.y, PositionMax.y, Random.value), Mathf.Lerp(PositionMin.z, PositionMax.z, Random.value)) });
       em.SetComponentData(e, new Rotation { Value = Quaternion.Euler(Mathf.Lerp(RotationMin.x, RotationMax.x, Random.value), Mathf.Lerp(RotationMin.y, RotationMax.y, Random.value), Mathf.Lerp(RotationMin.z, RotationMax.z, Random.value)) });
