e2c87ba6f77d267c595503e6062111ba985eb94f
Momin Khan <foolmoron@gmail.com>
2019-04-27 23:58:47 -0400

    On the way to earth scrolling

--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -1,14 +1,6 @@
 ﻿using Unity.Entities;
 using UnityEngine;
 
-public class Earth : MonoBehaviour, IConvertGameObjectToEntity {
-
-    [Range(0, 100)]
-    public float GravityAcceleration = 10;
-    public float MinRadius = 1;
-    public float MaxRadius = 10;
-
-    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
-        dstManager.AddComponentData(entity, new GravitySource { Acceleration = GravityAcceleration, MinRadius = MinRadius, MaxRadius = MaxRadius, });
-    }
+public class Earth : MonoBehaviour {
+    
 }
---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour {
+
+    public Vector2 Offset;
+    public Vector2 Speed;
+    Renderer renderer;
+
+    void Awake() {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        Offset.x = (Offset.x + Speed.x) % 1;
+        Offset.y = (Offset.y + Speed.y) % 1;
+        renderer.sharedMaterial.SetFloat("_OffsetX", Offset.x);
+        renderer.sharedMaterial.SetFloat("_OffsetY", Offset.y); // TODO: copy sprite shader and add offsets
+    }
+
+}
